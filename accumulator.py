23e77d0ed369a4a2487ab377a05e12b508020145
Adam Hopkins <admhpkns@gmail.com>
2018-03-21 10:42:23 +0200

    Add note about development

--- accumulator.py
+++ accumulator.py
@@ -10,6 +10,13 @@ async def fetch_data(url: str) -> None:
 
 
 if __name__ == '__main__':
+    """
+    THIS IS FOR DEVELOPMENT PURPOSES ONLY
+    =====================================
+
+    In production, the loop and fetch_data will be callable from other scripts,
+    for example, a Celery task. This file should not itself be executable.
+    """
     asyncio.set_event_loop(uvloop.new_event_loop())
     fetch = fetch_data('git@github.com:ahopkins/sanic-jwt.git')
     loop = asyncio.get_event_loop()
